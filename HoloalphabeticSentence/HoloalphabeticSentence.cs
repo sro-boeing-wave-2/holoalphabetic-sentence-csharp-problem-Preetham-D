3342abb748dacb2e7bd1cb3ca028d7a5739dae7d
Preetham D <preethamdbangera@gmail.com>
2018-07-19 11:43:48 +0530

    Solved using regex

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace HoloalphabeticSentence
 {
@@ -8,7 +10,21 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            return false;
+            Regex reg = new Regex("[^A-Z]");
+            HashSet<char> distinct = new HashSet<char>();
+            foreach (char x in reg.Replace(input.ToUpper(), ""))
+            {
+                distinct.Add(x);
+            }
+            return ((distinct.Count) == 26)? true : false;
+            //if (distinct.Count == 26)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
         }
     }
 }
